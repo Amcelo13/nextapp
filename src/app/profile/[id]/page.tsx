134e99c3c6373b6f2985751dfcb0f3756dc86ce2
chetelise <chetan.1129@zenmonk.tech>
2023-11-02 12:54:43 +0530

    dom

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,11 +1,18 @@
 'use client'
-import { useParams } from "next/navigation"
 
+import { useParams, useRouter } from "next/navigation"
 const MyProfile = () => {
+  const router = useRouter()
   const params = useParams()
   console.log('params----> ', params);
   return (
-    <div>My Profile ID - {params.id}</div>
+    <div>
+      <div>My Profile ID - {params.id}</div>
+      <h2 className="cursor-pointer" onClick={() => router.push('/profile')}>
+        
+        Back to Main profile page</h2>
+    </div>
+
   )
 }
 
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from "next/link";
 import { useRouter } from "next/navigation"
 const Profile = () => {
   const router = useRouter()
@@ -8,10 +9,11 @@ const Profile = () => {
     <div>
       <h1>My list of Profiles</h1>
       <div>
-        <ul>
-          <li>Profile 1</li>
-          <li>Profile 2</li>
-          <li>Profile 3</li>
+        <ul className="cursor-pointer">
+          {/* Pushing into Params Object of the url so that we can use it in the page */}
+          <Link href='/profile/1'>Profile 1</Link>
+          <li onClick={()=> router.push('/profile/2')}>Profile 2</li>
+          <li onClick={()=> router.push('/profile/3')}>Profile 3</li>
         </ul>
       </div>
     </div>
